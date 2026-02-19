5775b6a7231bcc52a421dd9f2df8cc791e36395d
Ian <ibede@byu.edu>
2026-02-19 16:30:36 -0700

    Add CopiedToPlex field and validation

--- Models/Movie.cs
+++ Models/Movie.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
-using Microsoft.EntityFrameworkCore;
 
 namespace Mission06_Bede.Models;
 
@@ -13,23 +10,28 @@ public partial class Movie
 
     public int? CategoryId { get; set; }
 
+    [Required]
     public string Title { get; set; } = null!;
 
+    [Required]
+    [Range(1888, int.MaxValue, ErrorMessage = "Year must be 1888 or later.")]
     public int Year { get; set; }
 
     public string? Director { get; set; }
 
     public string? Rating { get; set; }
 
+    [Required]
     public int Edited { get; set; }
 
     public string? LentTo { get; set; }
 
+    [Required]
     public int CopiedToPlex { get; set; }
 
+    [StringLength(25)]
     public string? Notes { get; set; }
 
     [ForeignKey("CategoryId")]
-    [InverseProperty("Movies")]
     public virtual Category? Category { get; set; }
 }
